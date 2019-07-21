9061053329f549aca5f2e67aa0f7dba6cbccbe7d
Carl Keneth Baseleres <baseleres.ck@gmail.com>
2019-07-21 17:21:21 +0800

    Added messaging functionality

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -74,7 +74,8 @@ namespace DatingApp.API.Controllers
     [HttpPost]
     public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDTO messageForCreationDTO)
     {
-      if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+      var sender = await _repo.GetUser(userId);
+      if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
         return Unauthorized();
 
       messageForCreationDTO.SenderId = userId;
@@ -87,14 +88,62 @@ namespace DatingApp.API.Controllers
       var message = _mapper.Map<Message>(messageForCreationDTO);
 
       _repo.Add(message);
-      var messageToReturn = _mapper.Map<MessageForCreationDTO>(message);
 
       if (await _repo.SaveAll())
+      {
+        var messageToReturn = _mapper.Map<MessageToReturnDTO>(message);
         return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
+      }
 
       throw new Exception("Creating the message failed on save");
     }
 
+    [HttpPost("{id}")]
+    public async Task<IActionResult> DeleteMessage(int id, int userId)
+    {
+      if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+        return Unauthorized();
+
+      var messageFromRepo = await _repo.GetMessage(id);
+
+      if (messageFromRepo.SenderId == userId)
+        messageFromRepo.SenderDeleted = true;
+
+      if (messageFromRepo.RecipientId == userId)
+        messageFromRepo.RecipientDeleted = true;
+
+      if (messageFromRepo.SenderDeleted && messageFromRepo.RecipientDeleted)
+        _repo.Delete(messageFromRepo);
+
+      if (await _repo.SaveAll())
+        return NoContent();
+
+      throw new Exception("Error deleting the message");
+    }
+
+    [HttpPost("{id}/read")]
+    public async Task<IActionResult> MarkMessageAsRead(int userId, int id)
+    {
+      if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+        return Unauthorized();
+
+      var message = await _repo.GetMessage(id);
+
+      if (message.RecipientId != userId)
+      {
+        return Unauthorized();
+      }
+
+      message.isRead = true;
+      message.DateRead = DateTime.Now;
+
+      await _repo.SaveAll();
+
+      return NoContent();
+    }
+
   }
+
+
 
 }
